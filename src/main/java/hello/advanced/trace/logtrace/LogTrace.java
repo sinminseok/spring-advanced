0d62f8032738843b9c95cb3f7f00c0d8b78cec14
sinminseok12 <sinminseok12@naver.com>
2024-03-06 11:29:22 +0900

    feat : ThreadLocal 적용

--- src/main/java/hello/advanced/trace/logtrace/LogTrace.java
+++ src/main/java/hello/advanced/trace/logtrace/LogTrace.java
@@ -7,4 +7,5 @@ public interface LogTrace {
     TraceStatus begin(String message);
     void end(TraceStatus status);
     void exception(TraceStatus status, Exception e);
+
 }
--- src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
+++ src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
@@ -4,20 +4,18 @@ import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
-public class FieldLogTrace implements LogTrace{
-
+public class ThreadLocalLogTrace implements LogTrace{
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX = "<X-";
 
-    private TraceId traceIdHolder; // traceId 동기화, 동시성 이슈 발생
+    private ThreadLocal<TraceId> traceIdHolder = new ThreadLocal<>();
 
     @Override
     public TraceStatus begin(String message) {
         syncTraceId();
-        TraceId traceId = traceIdHolder;
+        TraceId traceId = traceIdHolder.get();
         Long startTimeMs = System.currentTimeMillis();
         log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX,
                 traceId.getLevel()), message);
@@ -34,10 +32,10 @@ public class FieldLogTrace implements LogTrace{
         complete(status, e);
     }
 
-    private void complete(TraceStatus status, Exception e){
+    private void complete(TraceStatus status, Exception e) {
         Long stopTimeMs = System.currentTimeMillis();
         long resultTimeMs = stopTimeMs - status.getStartTimeMs();
-        TraceId traceId =  status.getTraceId();
+        TraceId traceId = status.getTraceId();
         if (e == null) {
             log.info("[{}] {}{} time={}ms", traceId.getId(),
                     addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(),
@@ -49,31 +47,21 @@ public class FieldLogTrace implements LogTrace{
         }
         releaseTraceId();
     }
-
-    /*
-    - TraceId 를 새로 만들거나 앞선 로그의 TraceId를 참고해서 동기화하고, level도 장가한다.
-    - 최초 호출이면 TraceId를 새로 만든다.
-    - 직전 로그가 있으면 해당 로그의 TraceId를 참고해서 동기화 하고, level 도 증가한다.
-    - 결과를 traceHolder 에 보관한다.
-     */
     private void syncTraceId() {
-        if(traceIdHolder == null){
-            traceIdHolder = new TraceId();
-        }else{
-            traceIdHolder = traceIdHolder.createNextId();
+        TraceId traceId = traceIdHolder.get();
+        if (traceId == null) {
+            traceIdHolder.set(new TraceId());
+        } else {
+            traceIdHolder.set(traceId.createNextId());
         }
     }
 
-    /*
-     - 메서드를 추가로 호출할 때는 level 이 하나 증가해야 하지만, 메서드 로출이 끝나면 level 이 하나 감소해야 한다.
-     - releaseTraceId() 는 level 을 하나 감소한다.
-     - 만약 최초 호출 (level == 0) 이면 내부에서 관리하는 traceId 를 제거한다.
-     */
-    private void releaseTraceId(){
-        if(traceIdHolder.isFirstLevel()){
-            traceIdHolder = null;
-        }else{
-            traceIdHolder = traceIdHolder.createPreviousId();
+    private void releaseTraceId() {
+        TraceId traceId = traceIdHolder.get();
+        if (traceId.isFirstLevel()) {
+            traceIdHolder.remove();//destroy
+        } else {
+            traceIdHolder.set(traceId.createPreviousId());
         }
     }
 
